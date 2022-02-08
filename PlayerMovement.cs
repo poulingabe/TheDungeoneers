50ad1d2a8319252c52d75a23ff85824195b39966
AlexandraGale <77981904+AlexandraGale@users.noreply.github.com>
2022-02-08 18:27:03 -0500

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   public float moveSpeed = 100f;
+
+   public Rigidbody playerRb; 
+  
+   Vector3 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+
+
+    }
+    void FixedUpdate()
+    {
+        //movement
+        playerRb.MovePosition((playerRb.position + movement * moveSpeed)); //* Time.fixedDeltaTime);
+
+    }
+}
